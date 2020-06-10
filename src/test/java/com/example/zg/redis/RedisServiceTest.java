da944c800f670d57dab2e8a47cfd55ec8b4fd114
zhangguo <2451666365@qq.com>
2020-06-10 17:50:04 +0800

    test

--- src/test/java/com/example/zg/redis/RedisServiceTest.java
+++ src/test/java/com/example/zg/redis/RedisServiceTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 import java.util.*;
 
 /**
@@ -85,68 +84,5 @@ public class RedisServiceTest {
 //        redisService.hdel("redisTestHash-zhangguo", "1");
         System.out.println(redisService.hHasKey("redisTestHash-zhangguo", "2"));
     }
-
-
-//    @Resource
-//    private StringRedisService<Person> stringRedisService;
-//
-//    @Test
-//    public void testStringRedis() {
-//        stringRedisService.set("redisTestValue", PERSON2);
-//        Person person = stringRedisService.get("redisTestValue", Person.class);
-//        System.out.println(person);
-//    }
-
-//    @Test
-//    public void testStringCommon() {
-//        System.out.println("redisTestHash：" + stringRedisService.getExpire("redisTestHash"));
-//        stringRedisService.del("redisTestHash");
-//        System.out.println(stringRedisService.hasKey("redisTestHash"));
-////        System.out.println("redisTestList-zhangguo：" + redisService.getExpire("redisTestList-zhangguo"));
-////        System.out.println("redisTestHash-zhangguo：" + redisService.getExpire("redisTestHash-zhangguo"));
-//    }
-//
-
-//
-//    @Test
-//    public void testStringReidsHash() {
-//        Map<String, Person> map = new HashMap<>();
-//        map.put("1", PERSON1);
-//        map.put("2", PERSON2);
-////        stringRedisService.hset("redisTestHash", "1", PERSON1);
-////        stringRedisService.hset("redisTestHash", "2", PERSON2);
-////        Person person = stringRedisService.hget("redisTestHash", "1", Person.class);
-////        System.out.println(person);
-////        stringRedisService.hmset("redisTestHash", map);
-////        stringRedisService.hmset("redisTestHash", map, 60);
-////        Map<String, Person> map1 = stringRedisService.hmget("redisTestHash", Person.class);
-////        System.out.println(map1);
-//        System.out.println(stringRedisService.hHasKey("redisTestHash", "3"));
-//        stringRedisService.hdel("redisTestHash", "1", "2");
-//    }
-//
-//    @Test
-//    public void testStringRedisList() {
-////        stringRedisService.lSet("redisTestList", PERSON_LIST);
-//        List<Person> people = stringRedisService.lGet("redisTestList", 0, -1, Person.class);
-//        System.out.println(stringRedisService.lGetListSize("redisTestList"));
-//        Person person = stringRedisService.lGetIndex("redisTestList", 2, Person.class);
-//        System.out.println(person);
-//        System.out.println(people);
-////        stringRedisService.lSet("redisTestList", PERSON1);
-//        stringRedisService.lUpdateIndex("redisTestList", 2, PERSON1);
-//    }
-//
-//    @Test
-//    public void testStringRedisSet() {
-////        stringRedisService.sSet("redisTestSet", new HashSet<>(PERSON_LIST));
-//        System.out.println(stringRedisService.sHasKey("redisTestSet", new Person("zhangguo", 27)));
-//        Set<Person> personSet = stringRedisService.sGet("redisTestSet", Person.class);
-//        System.out.println(personSet);
-//        stringRedisService.sSet("redisTestSet", new Person("xushicong", 22), new Person("liyan", 18));
-//        System.out.println(stringRedisService.sGetSetSize("redisTestSet"));
-//        stringRedisService.sRemove("redisTestSet", new Person("xushicong", 22));
-//    }
-
 
 }
